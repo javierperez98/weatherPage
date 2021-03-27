0402416434a043647b5436f5cbd3da969f986a41
Javier Perez <javier21perez98@gmail.com>
2021-03-27 13:53:43 -0700

    added UVindex fetch function and info

--- script.js
+++ script.js
@@ -14,14 +14,16 @@ $("#search").submit(function(event){
     var userCity = $("#userInput").val();
     document.getElementById("search").reset();
     userCity = userCity.replace(/\s+/g, ',').toLowerCase();
-    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q="+ userCity + "&units=imperial&appid=080f42673958d9248cf81c7911a0770a";
-    
+    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q="+ 
+    userCity + "&units=imperial&appid=080f42673958d9248cf81c7911a0770a";
+
     fetch(requestUrl)
-    .then(function (response) {
-    console.log(response);
-    if (response.status === 200) {
-        response.json()
+    .then(function (weatherInfo) {
+    console.log(weatherInfo);
+    if (weatherInfo.status === 200) {
+        weatherInfo.json()
         .then(data => {
+            console.log(data);
             var weather = data.weather[0].icon;
             var icon = "http://openweathermap.org/img/wn/" + weather + "@2x.png";
             var temp = data.main.temp;
@@ -32,8 +34,20 @@ $("#search").submit(function(event){
             currentTemp.innerHTML = "Temperature: " + temp + " °F";
             currentHumid.innerHTML = "Humidity: " + humid + " %";
             currentWind.innerHTML = "Wind Speed: " + wind + " MPH";
-            currentCard.style.display = "block";
-            forecastCard.style.display = "block";
+
+            var uvURL ="http://api.openweathermap.org/data/2.5/uvi?lat="+ 
+            data.coord.lat+ "&lon=" +data.coord.lon+ "&appid=080f42673958d9248cf81c7911a0770a";
+            fetch(uvURL)
+            .then(function(uvInfo){
+                console.log(uvInfo);
+                uvInfo.json()
+                .then(data => {
+                    console.log(data.value)
+                    currentUV.innerHTML = "UV Index: " + data.value;
+                    currentCard.style.display = "block";
+                    forecastCard.style.display = "block";
+                })
+            })
         });
     }
     else {
