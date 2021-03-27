012de7855c7c6620294b4da9c52b956603a20f0b
Javier Perez <javier21perez98@gmail.com>
2021-03-27 10:53:35 -0700

    fetch command working for weather data

--- script.js
+++ script.js
@@ -2,7 +2,24 @@ $("#search").submit(function(event){
     event.preventDefault();
     var city = $("#userInput").val()
     document.getElementById("search").reset();
-    city = city.replace(/\s+/g, '-').toLowerCase();
-    var requestUrl = "api.openweathermap.org/data/2.5/weather?q="+ city + "&appid=080f42673958d9248cf81c7911a0770a";
-    console.log(requestUrl);
+    city = city.replace(/\s+/g, ',').toLowerCase();
+    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q="+ city + "&units=imperial&appid=080f42673958d9248cf81c7911a0770a";
+    
+    fetch(requestUrl)
+    .then(function (response) {
+    console.log(response);
+    if (response.status === 200) {
+        response.json()
+        .then(data => {
+            var icon = data.weather.icon;
+            var temp = data.main.temp;
+            var humid = data.main.humidity;
+            var wind = data.wind.speed;
+            
+        });
+    }
+    else {
+        return;
+    }    
+  });
 });
