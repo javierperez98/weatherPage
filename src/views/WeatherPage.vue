ca1469bf8b879da60e6463679bce15de386a5089
javierperez98 <javier21perez98@gmail.com>
2022-08-25 02:33:04 -0400

    main page

---
+++ src/views/WeatherPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <Transition appear>
+    <div>
+      <h1 class="text-light mb">Weather Forecast</h1>
+      <SearchBar />
+      <WeatherCard />
+    </div>
+  </Transition>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import WeatherCard from "@/components/WeatherCard.vue";
+import SearchBar from "@/components/SearchBar.vue";
+
+export default defineComponent({
+  name: "WeatherPage",
+  components: {
+    WeatherCard,
+    SearchBar,
+  },
+});
+</script>
+
+<style lang="scss">
+.mb {
+  margin-bottom: 1rem;
+}
+</style>
