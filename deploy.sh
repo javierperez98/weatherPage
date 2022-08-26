ce9d5c07b8ee3285c689a1d8c20805e80604fb6d
javierperez98 <javier21perez98@gmail.com>
2022-08-25 23:34:38 -0400

    create deploy files

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/user/bin/env sh
+
+set -e
+
+npm run build
+
+cd dist
+
+git init
+git add -A
+git commit -m "Deployed"
+git push -f git@github.com:javierperez98/weatherPage.git main:gh-pages
+
+cd -
